33d64418558458b3b062f4eb669f2da7ca5cc23b
sudeep <sudeepmw@gmail.com>
2020-02-07 03:41:35 -0500

    AndroidLayout#2

---
+++ app/src/main/java/com/bawp/myapplication/View_Items.java
@@ -0,0 +1,14 @@
+package com.bawp.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_Items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__items);
+    }
+}
