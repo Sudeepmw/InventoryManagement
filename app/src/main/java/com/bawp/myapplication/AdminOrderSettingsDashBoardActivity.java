4d50ddfb928ee93f914e558ba40b50957c8c2830
Vinay Rao <vinayrao9704@gmail.com>
2020-02-17 01:04:18 -0500

    //Admins View Orders//

---
+++ app/src/main/java/com/bawp/myapplication/AdminOrderSettingsDashBoardActivity.java
@@ -0,0 +1,54 @@
+package com.bawp.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminOrderSettingsDashBoardActivity extends AppCompatActivity {
+    Button btn_orders,btn_settings;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_order_settings_dashboard);
+
+        getSupportActionBar().setTitle("Admin Dashboard");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        btn_orders=(Button)findViewById(R.id.btn_orders);
+        btn_settings=(Button)findViewById(R.id.btn_settings);
+
+        btn_orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminOrderSettingsDashBoardActivity.this,AdminDashBoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminOrderSettingsDashBoardActivity.this,AdminSettingsDashboard.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
