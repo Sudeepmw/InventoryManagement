e8e5edd2d843aeb7021e1c44cc87694cfdf717f5
dineshreddy <dineshreddy2747@gmail.com>
2020-02-24 18:21:22 -0500

    Database connection URL

--- app/src/main/java/com/bawp/myapplication/DatabaseConnection/DatabaseBaseConnection.java
+++ app/src/main/java/com/bawp/myapplication/DatabaseConnection/DatabaseBaseConnection.java
@@ -1,4 +1,33 @@
 package com.bawp.myapplication.DatabaseConnection;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
 public class DatabaseBaseConnection {
-}
+
+        private static Retrofit result;
+
+
+
+        private static final String BASE_URL = "http://inventoryandorderingapp.com/";
+
+
+
+
+        public static Retrofit getResult() {
+
+
+
+            if (result == null) {
+                result = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+            }
+            return result;
+        }
+    }
+
+
