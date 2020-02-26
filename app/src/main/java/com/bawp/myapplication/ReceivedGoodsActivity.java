7d11c8c85c953e34315dab1cdd6765b1f1ff02bd
dineshreddy <dineshreddy2747@gmail.com>
2020-02-26 00:38:59 -0500

    ReceivedGoods Screen

---
+++ app/src/main/java/com/bawp/myapplication/ReceivedGoodsActivity.java
@@ -0,0 +1,14 @@
+package com.bawp.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceivedGoodsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_received_goods);
+    }
+}
