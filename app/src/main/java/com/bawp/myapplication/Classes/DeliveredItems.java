8f1c4b65812c3381409bb73d55ecae75c04075e6
nikk2 <nikk2412@gmail.com>
2020-02-28 00:08:38 -0500

    Delivered Orders List

---
+++ app/src/main/java/com/bawp/myapplication/Classes/DeliveredItems.java
@@ -0,0 +1,25 @@
+package com.bawp.myapplication.Classes;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DeliveredItems {
+
+
+    @SerializedName("id")
+    public String id;
+
+    @SerializedName("items")
+    public String items;
+
+    @SerializedName("quantity")
+    public String quantity;
+
+    @SerializedName("price")
+    public String price;
+
+    @SerializedName("order_date")
+    public String order_date;
+
+    @SerializedName("order_created_by")
+    public String order_created_by;
+}
