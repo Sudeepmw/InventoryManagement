703d15e914bb841471c3a75548fc324c293a28ae
nikk2 <nikk2412@gmail.com>
2020-02-28 00:05:04 -0500

    Update Items

---
+++ app/src/main/java/com/bawp/myapplication/Classes/UpdateItems.java
@@ -0,0 +1,39 @@
+package com.bawp.myapplication.Classes;
+
+import com.google.gson.annotations.SerializedName;
+
+public class UpdateItems {
+
+
+
+    @SerializedName("goods_name")
+    private String goods_name;
+
+    @SerializedName("quantity")
+    private String quantity;
+
+    public UpdateItems(String goods_name, String quantity) {
+        this.setGoods_name(goods_name);
+        this.setQuantity(quantity);
+
+
+
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getGoods_name() {
+        return goods_name;
+    }
+
+    public void setGoods_name(String goods_name) {
+        this.goods_name = goods_name;
+    }
+
+}
