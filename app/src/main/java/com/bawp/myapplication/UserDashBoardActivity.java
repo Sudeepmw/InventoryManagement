48baccec35e9d32d948f731460dfedd64a50e7d8
dineshreddy <dineshreddy2747@gmail.com>
2020-03-02 01:14:11 -0500

    //user dashBoard Logout//

--- app/src/main/java/com/bawp/myapplication/UserDashBoardActivity.java
+++ app/src/main/java/com/bawp/myapplication/UserDashBoardActivity.java
@@ -1,5 +1,8 @@
 package com.bawp.myapplication;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,8 +17,8 @@ import androidx.cardview.widget.CardView;
 
 
 public class UserDashBoardActivity extends AppCompatActivity {
-    // Button cd_order_goods,cd_receive_goods,cd_not_received,btn_viewmyorders;
     CardView cd_new_order,cd_order_history,cd_notifications,edit_myprofile;
+    ProgressDialog progressDialog;
 
 
     @Override
@@ -69,8 +72,9 @@ Intent intent=new Intent(UserDashBoardActivity.this,UserEditProfileActivity.clas
 
 
     }
+  @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds Items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);//Menu Resource, Menu
         return true;
     }
@@ -82,14 +86,46 @@ Intent intent=new Intent(UserDashBoardActivity.this,UserEditProfileActivity.clas
                 return true;
 
             case R.id.idlogout:
-                Intent i=new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(i);
-                finish();
+                // Toast.makeText(getApplicationContext(),"logout Selected",Toast.LENGTH_LONG).show();
 
-                return true;
+                final AlertDialog.Builder builder = new AlertDialog.Builder(UserDashBoardActivity.this);
+                builder.setMessage("Are you sure you want to logout?");
+                builder.setCancelable(true);
+                builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        progressDialog = new ProgressDialog(UserDashBoardActivity.this);
+                        progressDialog.setMessage("logging out....");
+                        progressDialog.show();
+
+                        Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(i);
+                        finish();
+
+                    }
+                });
+
+                builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+
+
+
             default:
                 return super.onOptionsItemSelected(item);
         }
+
+    }
+    public void onBackPressed(){
+        //  Toast.makeText(UserDashBoardActivity.this,"  ",Toast.LENGTH_LONG).show();
+
 
     }
 }
