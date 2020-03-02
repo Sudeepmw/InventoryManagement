8d61bced505fac77d66439559d5b827ab10bee7e
dineshreddy <dineshreddy2747@gmail.com>
2020-03-02 14:25:31 -0500

    // Add items Screen java //

--- app/src/main/java/com/inventorymanagementsystem/JavaClasses/AddItemsActivity.java
+++ app/src/main/java/com/inventorymanagementsystem/JavaClasses/AddItemsActivity.java
@@ -1,22 +1,220 @@
 package com.inventorymanagementsystem.JavaClasses;
 
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.inventorymanagementsystem.InventoryEndURL;
 import com.inventorymanagementsystem.R;
+import com.inventorymanagementsystem.ResponseData;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import pub.devrel.easypermissions.EasyPermissions;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-public class AddItemsActivity extends AppCompatActivity  {
-
+public class AddItemsActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    TextView tv_goods_name,tv_price,tv_quantity;
+    EditText et_goods_name,et_price,et_quantity;
+    Button select_image;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    String date;
+    Button btn_submit;
+    private static final String TAG = AddItemsActivity.class.getSimpleName();
+    private static final int REQUEST_GALLERY_CODE = 200;
+    private static final int READ_REQUEST_CODE = 300;
+    private static final String SERVER_PATH = "http://inventoryandorderingapp.com/";
+    private Uri uri;
+    ProgressDialog progressDialog;
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goods);
 
+        getSupportActionBar().setTitle("Add Item");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        btn_submit=(Button)findViewById(R.id.btn_submit);
+        select_image=(Button)findViewById(R.id.select_image);
+
+        tv_goods_name =(TextView)findViewById(R.id.tv_goods_name);
+        tv_price =(TextView)findViewById(R.id.tv_price);
+        tv_quantity=(TextView)findViewById(R.id.tv_quantity);
+
+        et_goods_name =(EditText) findViewById(R.id.et_goods_name);
+        et_price =(EditText) findViewById(R.id.et_price);
+        et_quantity=(EditText) findViewById(R.id.et_quantity);
+
+        Typeface fontstyle=Typeface.createFromAsset(getApplicationContext().getAssets(),"fonts/Lato-Medium.ttf");
+        tv_goods_name.setTypeface(fontstyle);
+        tv_price.setTypeface(fontstyle);
+        et_goods_name.setTypeface(fontstyle);
+        et_price.setTypeface(fontstyle);
+        btn_submit.setTypeface(fontstyle);
+        tv_quantity.setTypeface(fontstyle);
+        et_quantity.setTypeface(fontstyle);
+        select_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK);
+                openGalleryIntent.setType("image/*");
+                startActivityForResult(openGalleryIntent, REQUEST_GALLERY_CODE);
+            }
+        });
+
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (et_goods_name.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter Goods name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (et_price.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter Price", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (et_quantity.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter Quantity", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                uploadImageToServer();
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+            case R.id.idlogout:
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                finish();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);//Menu Resource, Menu
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, AddItemsActivity.this);
+    }
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_GALLERY_CODE && resultCode == Activity.RESULT_OK){
+            uri = data.getData();
+            if(EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                String filePath = getRealPathFromURIPath(uri, AddItemsActivity.this);
+                file = new File(filePath);
+
+            }else{
+                EasyPermissions.requestPermissions(this, getString(R.string.read_file), READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
+        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
+    }
+    File file;
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if(uri != null){
+            String filePath = getRealPathFromURIPath(uri, AddItemsActivity.this);
+            file = new File(filePath);
+            // uploadImageToServer();
+        }
+    }
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "Permission has been denied");
+
+    }
+    ProgressDialog pd;
+    private void uploadImageToServer(){
+        pd=new ProgressDialog(AddItemsActivity.this);
+        pd.setTitle("Loading");
+        pd.show();
+        Map<String, String> map = new HashMap<>();
+        map.put("item_name",et_goods_name.getText().toString());
+        map.put("price",et_price.getText().toString());
+        map.put("quantity",et_quantity.getText().toString());
+
+
+        RequestBody mFile = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("file", file.getName(), mFile);
+        RequestBody filename = RequestBody.create(MediaType.parse("text/plain"), file.getName());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(SERVER_PATH)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        InventoryEndURL uploadImage = retrofit.create(InventoryEndURL.class);
+        Call<ResponseData> fileUpload = uploadImage.add_items(fileToUpload, map);
+        fileUpload.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                pd.dismiss();
+                Toast.makeText(AddItemsActivity.this, "Data is submitted successfully. ", Toast.LENGTH_LONG).show();
+                finish();
+            }
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                pd.dismiss();
+                Toast.makeText(AddItemsActivity.this, "Error"+t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
